43f3b89c2877a8d628b820f768ccb2faa7649906
wqqlf <wqonlyfanmanager@gmail.com>
2026-06-19 01:37:54 +0200

    Fix

--- HmzHub/games/_template.lua
+++
@@ -1,3 +0,0 @@
-return function(H)
-	error("[HMZ Hub] Game module not implemented: copy games/anime_astral.lua and adapt")
-end
--- inf_yield.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
